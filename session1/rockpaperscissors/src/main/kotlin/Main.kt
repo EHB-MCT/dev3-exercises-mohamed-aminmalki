442e129095e5cc7b4b9d94be4dfbb03be05ad19d
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-12 11:38:36 +0200

    keuze kiezen

--- session1/rockpaperscissors/src/main/kotlin/Main.kt
+++ session1/rockpaperscissors/src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
 fun main(){
-    val optie = arrayOf("schaar", "steen", "papier")
-    val computerKeuze = optie.random()
+    val options = arrayOf("schaar", "steen", "papier")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("computer picked $computerChoice")
+    println("user picked $userChoice")
+
+    determineWinner(computerChoice, userChoice)
+}
+
+fun getUserChoice(options: Array<String>): String?{
+    var userChoice: String?
+    do{
+        println("Make your choice: rock - paper or scissors")
+        userChoice = readLine()
+    }while (!options.contains(userChoice))
+
+    return userChoice
+}
+
+fun determineWinner(computerChoice: String, userChoice: String?){
 
 }
