7e82a050ed73125c61353b44224d2502f547c83a
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-12 11:45:37 +0200

    final game

--- session1/rockpaperscissors/src/main/kotlin/Main.kt
+++ session1/rockpaperscissors/src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main(){
-    val options = arrayOf("schaar", "steen", "papier")
+    val options = arrayOf("rock", "paper", "scissors")
     val computerChoice = options.random()
     val userChoice = getUserChoice(options)
 
-    println("computer picked $computerChoice")
-    println("user picked $userChoice")
+    println("Computer picked $computerChoice")
+    println("User picked $userChoice")
 
     determineWinner(computerChoice, userChoice)
 }
@@ -20,5 +20,18 @@ fun getUserChoice(options: Array<String>): String?{
 }
 
 fun determineWinner(computerChoice: String, userChoice: String?){
+    if(computerChoice == userChoice){
+        println("It's a draw")
+        return
+    }
 
+    val winMessage = "You 've won"
+    val loseMessage = "You 've lost"
+
+    when (computerChoice){
+        "rock" -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
+        "scissors" -> if (userChoice == "rock") println(winMessage) else println(loseMessage)
+
+    }
 }
