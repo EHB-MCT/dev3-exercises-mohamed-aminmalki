71e6dd72b8e0b2938f07a8a8d062667b7b27f95e
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-19 17:44:35 +0200

    chatroulette classes

---
+++ session3/chatroulette/src/main/kotlin/Comedian.kt
@@ -0,0 +1,5 @@
+class Comedian  (override val name: String): Person(name){
+    fun tellJoke(){
+        println("")
+    }
+}
---
+++ session3/chatroulette/src/main/kotlin/Student.kt
@@ -0,0 +1,5 @@
+class Student (override val name: String): Person(name) {
+    fun gossip(){
+
+    }
+}
